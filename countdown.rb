dac87f9ad4866e5d7617aa72ba14a48efbd85e91
Eric J Wilhite <ericjwilhite@gmail.com>
2019-06-05 21:20:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,11 @@
 #write your code here
 
-def countdown
-  counter = 0
-  while counter < 1-
+def countdown(number)
+  while number >= 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  end
+end
+
+def countdown_with_sleep
 end
